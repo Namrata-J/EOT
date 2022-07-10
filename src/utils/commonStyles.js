4ba33ecc1fe9bf596d68a1452164c5d47e325f7f
Namrata-J <namratajain29001@gmail.com>
2022-07-10 20:54:47 +0530

    utility: added utility styles

---
+++ src/utils/commonStyles.js
@@ -0,0 +1,48 @@
+export const flexCenter = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+export const horizontalFlexCenter = {
+    display: 'flex',
+    justifyContent: 'center'
+}
+
+export const verticalFlexCenter = {
+    display: 'flex',
+    alignItems: 'center'
+}
+
+export const flexBetweenVariant1 = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around'
+}
+
+export const fullScreen = {
+    width: '100%',
+    height: '100vh'
+}
+
+export const containerType1 = {
+    width: '95%',
+    height: '90vh',
+}
+
+export const darkOpacity = {
+    bgcolor: 'secondary.main',
+    opacity: '0.5',
+}
+
+export const headingVariantForLandingPage = {
+    display: 'inline-block',
+    fontWeight: 'bold',
+    color: 'otherColors.white',
+    fontSize: { xs: 'x-small', sm: 'small', md: 'medium', lg: 'large' },
+    marginBottom: '0.5rem'
+}
+
+export const landingPageBtn = {
+    fontSize: { xs: 'x-small', sm: 'small' }
+}
