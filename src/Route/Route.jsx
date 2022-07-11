f5901761e05d3754881125aeb6263fa94c1b789a
Namrata-J <namratajain29001@gmail.com>
2022-07-11 23:16:18 +0530

    added routes for all the new introduced pages

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import { LandingPage } from "../screens/";
+import { LandingPage, HomePage, ExplorePage, BookmarkPage, NotificationPage, ProfilePage, Auth } from "../screens/";
 
 const RouteComp = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/explore" element={<ExplorePage />} />
+                <Route path="/bookmark" element={<BookmarkPage />} />
+                <Route path="/notification" element={<NotificationPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/signup" element={<Auth.SignUpPage />} />
+                <Route path="/login" element={<Auth.LogInPage />} />
+                <Route path="/logout" element={<Auth.LogOutPage />} />
             </Routes>
         </div>
     );
