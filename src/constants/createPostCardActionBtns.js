7ecd26997dd7c9bf761961c355a88936461f9b4e
Namrata-J <namratajain29001@gmail.com>
2022-07-21 23:00:31 +0530

    mock data to db post file in backend foldre

--- src/constants/createPostCardActionBtns.js
+++ src/constants/createPostCardActionBtns.js
@@ -1,15 +1,10 @@
 import SentimentSatisfiedTwoToneIcon from '@mui/icons-material/SentimentSatisfiedTwoTone';
 import PhotoCameraBackTwoToneIcon from '@mui/icons-material/PhotoCameraBackTwoTone';
-import GifBoxTwoToneIcon from '@mui/icons-material/GifBoxTwoTone';
 
 export const createPostCardActionBtns = [
     {
         icon: <PhotoCameraBackTwoToneIcon />,
         iconName: "UPLOAD-MEDIA"
-    },
-    {
-        icon: <GifBoxTwoToneIcon />,
-        iconName: "UPLOAD_GIF"
     },
     {
         icon: <SentimentSatisfiedTwoToneIcon />,
