c8cfb0881cbf07edf8562388f041884dfba1d884
Namrata-J <namratajain29001@gmail.com>
2022-07-31 12:32:28 +0530

    feat: notification screen

--- src/screens/Notification.js
+++ src/screens/Notification.js
@@ -1,6 +1,31 @@
+import { pageBoxStyling, scrollbar, sectionSpacing } from "../utils/commonStyles";
+import { utilComp } from "../components/";
+import { Box, Typography } from '@mui/material';
+
 const NotificationPage = () => {
     return (
-        <div>NotificationPage</div>
+        <utilComp.PageContainer>
+            <utilComp.Header />
+            <Box sx={pageBoxStyling}>
+                <utilComp.SideBar />
+                <Box
+                    sx={{
+                        ...scrollbar,
+                        width: '45rem',
+                        ...sectionSpacing,
+                        position: 'relative'
+                    }}>
+                    <Typography sx={{
+                        textAlign: 'center',
+                        pt: 10,
+                        fontSize: { xs: '1rem', md: '1.2rem' }
+                    }}>
+                        No notifications yet
+                    </Typography>
+                </Box>
+                <utilComp.FollowRecommendationSection />
+            </Box>
+        </utilComp.PageContainer>
     );
 }
 
